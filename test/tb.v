02ac4c23d8594b722bd71a034f4f285a1122a782
Timothy Gu <timyugu@gmail.com>
2025-07-19 18:32:53 -0400

    dumpvars cocotb fix

--- test/tb.v
+++ test/tb.v
@@ -9,7 +9,7 @@ module tb ();
   // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
+    $dumpvars(0, tb.user_project.u_cpu_core);
     #1;
   end
 
